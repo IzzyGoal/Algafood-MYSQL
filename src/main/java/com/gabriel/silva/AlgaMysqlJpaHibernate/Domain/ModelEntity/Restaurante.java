c8283d997859c82f42d27253bdb47a5b3f779ee9
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-06-29 11:52:44 -0300

    Alteração e correção na conexão com Mysql

--- src/main/java/com/gabriel/silva/AlgaMysqlJpaHibernate/Domain/ModelEntity/Restaurante.java
+++ src/main/java/com/gabriel/silva/AlgaMysqlJpaHibernate/Domain/ModelEntity/Restaurante.java
@@ -1,2 +1,57 @@
-package com.gabriel.silva.AlgaMysqlJpaHibernate.Domain.ModelEntity;public class Restaurante {
+package com.gabriel.silva.AlgaMysqlJpaHibernate.Domain.ModelEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Restaurante {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nom_restaurante")
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurante that = (Restaurante) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
